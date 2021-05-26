2b1df0e6a1e72696406a6f755080d33ef1b74370
c2ireton <c2ireton@gmail.com>
2021-05-26 01:09:53 +0000

    turning in for grade

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+
+
+
+//used to set/show results of slide movement and selection
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rateval;
+}
+
+
+//compute total amount of money 
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+//validate user input to make sure a positive number is entered
+    if(principal < 1) 
+    {
+        alert("Enter A positive number");
+        document.getElementById("principal").focus();
+    }
+    else
+    {
+    //prints out results of computing the user input
+        document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>.\<br\>Your will recieve an amount of \<mark\>"+interest+"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>";
     
+    } 
+
+
 }
         
